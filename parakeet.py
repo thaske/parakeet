7f564e0735b4f57d5fabcca9d97ec9d6b7bc93b5
Evan <me@thaske.com>
2017-05-11 13:56:49 -0400

    Added basic functionality

---
+++ parakeet.py
@@ -0,0 +1,31 @@
+import praw, re
+
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit('mechmarket')
+item = 'HHKB'
+low = 25
+high = 300
+
+for submission in subreddit.stream.submissions():
+
+    # process titles to only include items that are being sold
+    if '[H]' in submission.title:
+        items = re.findall(r'\[H\](.*?)\[W\]', submission.title)
+        items = "".join(items).replace('\n','').lstrip()
+        # print(items)    # debugging purposes
+
+    # shorten url
+    url = f'reddit.com/{submission.id}'
+
+    # find posts containing the item
+    if item in items:
+        print('===============================')
+        print(f'Found a submission!\nTitle: {submission.title}\nURL: {url}\n')
+
+        prices = re.findall(r'\d+', submission.selftext)    # finds all numbers in the selftext
+        print('Possible Price:')
+        for price in prices:
+            if int(price) > low and int(price) < high:        # general range for
+                print(f'${price}')
+
+        print('===============================\n\n')
