20ca0b11682448578faff53e302488beffac42dc
Evan <me@thaske.com>
2017-05-11 22:24:08 -0400

    Seperated processing code into its own function

--- parakeet.py
+++ parakeet.py
@@ -1,18 +1,21 @@
 import praw, re, datetime
 
-reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit('mechmarket')
-item = 'HHKB'
-low = 25
-high = 300
+def main():
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit('mechmarket')
 
-for submission in subreddit.stream.submissions():
+    item = 'Alps'
+    low = 0
+    high = 200
+
+    for submission in subreddit.stream.submissions():
+        process_submission(submission, item, low, high)
+
+def process_submission(submission, item, low, high):
 
     # process titles to only include items that are being sold
-    if '[H]' in submission.title:
-        items = re.findall(r'\[H\](.*?)\[W\]', submission.title)
-        items = "".join(items).replace('\n','').lstrip()
-        # print(items)    # debugging purposes
+    items = re.findall(r'\[H\](.*?)\[W\]', submission.title)
+    items = "".join(items).replace('\n','').lstrip()
 
     # shorten url
     url = f'reddit.com/{submission.id}'
@@ -23,10 +26,13 @@ for submission in subreddit.stream.submissions():
         print('===============================')
         print(f'Found a submission!\nTitle: {submission.title}\nTime: {time}\nURL: {url}\n')
 
-        prices = re.findall(r'\d+', submission.selftext)    # finds all numbers in the selftext
+        prices = re.findall(r'\d+(?<!%)', submission.selftext)    # finds all numbers in the selftext
         print('Possible Price:')
         for price in prices:
-            if int(price) > low and int(price) < high:        # general range for
+            if int(price) > low and int(price) < high:        # only display prices that would be reasonable for the item
                 print(f'${price}')
 
         print('===============================\n\n')
+
+if __name__ == '__main__':
+    main()
