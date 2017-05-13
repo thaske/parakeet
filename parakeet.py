d49a9f5a94cfab8f1708b217c607cf177a9d2db7
Evan <me@thaske.com>
2017-05-13 02:24:35 -0400

    Fixed Pushover authentication with configparser

--- parakeet.py
+++ parakeet.py
@@ -1,21 +1,23 @@
 from chump import Application
-import praw, re, datetime
+import praw, re, datetime, configparser
 
 def main():
     reddit = praw.Reddit('bot1')
     subreddit = reddit.subreddit('mechmarket')
 
-    app = Application()     # app token
-    user = app.get_user()   # user token
+    config = configparser.ConfigParser()
+    config.read("pushover.ini")
+    app = Application(config.get('Pushover', 'app_key'))     # app token
+    user = app.get_user(config.get('Pushover', 'user_key'))   # user token
 
     item = 'HHKB'
-    low = 100
+    low = 0
     high = 250
 
     for submission in subreddit.stream.submissions():
 
          l = process_submission(submission, item, low, high)
-         if l[0] == True:
+         if l[0]:
              message = user.send_message(
                  title = l[1],
                  message =  l[2],
@@ -31,13 +33,12 @@ def process_submission(submission, item, low, high):
     items = re.findall(r'\[H\](.*?)\[W\]', submission.title)
     items = "".join(items).replace('\n','').lstrip()
 
-    title = f'Found {item}'
-    time = datetime.datetime.fromtimestamp(submission.created)
-    msg = f'<div>{submission.title}</div><div>{time}</div>'
-    url = f'reddit.com/{submission.id}'
-
     if item in items:
+        title = f'Found {item}'
+        msg = f'<div>{items}</div>'
+        url = f'reddit.com/{submission.id}'
         return [True, title, msg, url]
+
     else:
         return [False]
 
